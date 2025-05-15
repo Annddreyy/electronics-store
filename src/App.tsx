c867c34519f530db6de1f10b5e4edc23f1efdf75
Andrey22 <andreymail22112006@gmail.com>
2025-05-15 14:14:25 +0300

    feat: #28 Сверстать страницу гарантийных условий

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import { Main } from './pages/Main';
 import { AboutUs } from './pages/AboutUs';
+import { WarrantyConditions } from './pages/WarrantyConditons';
 
 const App: React.FC = () => {
     return (
         <Routes>
             <Route path="/" element={<Main />} />
             <Route path="/about-us" element={<AboutUs />} />
+            <Route
+                path="/warranty-conditions"
+                element={<WarrantyConditions />}
+            />
         </Routes>
     );
 };
