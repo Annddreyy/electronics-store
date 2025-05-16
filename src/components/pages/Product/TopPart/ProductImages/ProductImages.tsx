509130ba67f69a8e057cc4ba62dd9735970cf4cc
Andrey22 <andreymail22112006@gmail.com>
2025-05-16 16:18:19 +0300

    feat: #33 Сделать слайдер избражений

--- src/components/pages/Product/TopPart/ProductImages/ProductImages.tsx
+++ src/components/pages/Product/TopPart/ProductImages/ProductImages.tsx
@@ -1,18 +1,58 @@
-import img from './../../../../../assets/productDefault.png';
+import { useState } from 'react';
 import classes from './ProductImages.module.scss';
+import cn from 'classnames';
 
-export const ProductImages: React.FC = () => {
+type PropsType = {
+    images: string[];
+};
+
+export const ProductImages: React.FC<PropsType> = ({ images }) => {
+    const [imageIndex, setImageIndex] = useState<number>(0);
+    const imagesElem = images.map((img, index) => (
+        <img
+            src={img}
+            alt=""
+            data-selected={index === imageIndex || undefined}
+        />
+    ));
+
+    const onPrevImageHandler = () => {
+        setImageIndex((prev) => (prev > 0 ? prev - 1 : prev));
+    };
+
+    const onNextImageHandler = () => {
+        setImageIndex((prev) => (prev < images.length - 1 ? prev + 1 : prev));
+    };
+
     return (
         <div className={classes.productImages}>
-            <img src={img} alt="" />
+            <img
+                src={images[imageIndex]}
+                className={classes.selectedImg}
+                alt=""
+            />
             <div className={classes.slider}>
-                <button>&lt;</button>
-                <div className={classes.images}>
-                    <img src={img} alt="" />
-                    <img src={img} alt="" />
-                    <img src={img} alt="" />
-                </div>
-                <button>&gt;</button>
+                <button
+                    onClick={onPrevImageHandler}
+                    disabled={imageIndex === 0}
+                    className={cn('border-secondary', {
+                        'bg-secondary': imageIndex !== 0,
+                        'text-secondary': imageIndex === 0,
+                    })}
+                >
+                    ◀
+                </button>
+                <div className={classes.images}>{imagesElem}</div>
+                <button
+                    onClick={onNextImageHandler}
+                    disabled={imageIndex === images.length - 1}
+                    className={cn('border-secondary', {
+                        'bg-secondary': imageIndex !== images.length - 1,
+                        'text-secondary': imageIndex === images.length - 1,
+                    })}
+                >
+                    ▶
+                </button>
             </div>
         </div>
     );
--- src/components/pages/Product/TopPart/TopPart.tsx
+++ src/components/pages/Product/TopPart/TopPart.tsx
@@ -2,11 +2,13 @@ import { BuyConditions } from './BuyConditions/BuyConditions';
 import { MainInformation } from './MainInformation/MainInformation';
 import { ProductImages } from './ProductImages/ProductImages';
 import classes from './TopPart.module.scss';
+import img1 from './../../../../assets/card/card.png';
+import img2 from './../../../../assets/productDefault.png';
 
 export const TopPart: React.FC = () => {
     return (
         <section className={classes.topPart}>
-            <ProductImages />
+            <ProductImages images={[img1, img2]} />
             <div className={classes.productMainInformation}>
                 <h1 className="pageTitle">
                     Гироскутер Smart Balance Well 6.5 Хип-Хоп (АКВАЗАЩИТА)
