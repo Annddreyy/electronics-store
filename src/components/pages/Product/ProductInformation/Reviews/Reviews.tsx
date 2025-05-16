cb034ead1544c47937d2bf9710133e5143ca74a6
Andrey22 <andreymail22112006@gmail.com>
2025-05-16 16:33:35 +0300

    feat: #33 Обновить блок отзывов

--- src/components/pages/Product/ProductInformation/Reviews/Reviews.tsx
+++ src/components/pages/Product/ProductInformation/Reviews/Reviews.tsx
@@ -1,4 +1,5 @@
 import { Review } from '../../../../common/Review/Review';
+import classes from './Reviews.module.scss';
 
 export const Reviews: React.FC = () => {
     return (
@@ -6,13 +7,43 @@ export const Reviews: React.FC = () => {
             <h2 className="sectionTitle">
                 Отзывы на гироскутер Smart Balance Well 6.5
             </h2>
-            <Review
-                authorName="Александр"
-                date={new Date(2021, 5, 7)}
-                grade={4}
-                title="Оличный самокат!"
-                text="Катаюсь каждый день после работы, заряд держит отлично!"
-            />
+            <div className={classes.reviewsBottom}>
+                <div className={classes.reviews}>
+                    <Review
+                        authorName="Александр"
+                        date={new Date(2021, 5, 7)}
+                        grade={4}
+                        title="Оличный самокат!"
+                        text="Катаюсь каждый день после работы, заряд держит отлично!"
+                    />
+                    <Review
+                        authorName="Александр"
+                        date={new Date(2021, 5, 7)}
+                        grade={4}
+                        title="Оличный самокат!"
+                        text="Катаюсь каждый день после работы, заряд держит отлично!"
+                    />
+                    <Review
+                        authorName="Александр"
+                        date={new Date(2021, 5, 7)}
+                        grade={4}
+                        title="Оличный самокат!"
+                        text="Катаюсь каждый день после работы, заряд держит отлично!"
+                    />
+                    <Review
+                        authorName="Александр"
+                        date={new Date(2021, 5, 7)}
+                        grade={4}
+                        title="Оличный самокат!"
+                        text="Катаюсь каждый день после работы, заряд держит отлично!"
+                    />
+                </div>
+                <div className={classes.addReview}>
+                    <strong>Напишите свое мнение о товаре</strong>
+                    <p>Сделайте выбор других покупателей легче</p>
+                    <button className="button-primary">Написать отзыв</button>
+                </div>
+            </div>
         </div>
     );
 };
