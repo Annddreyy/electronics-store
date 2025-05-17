d16b23304b6b37867c39f316fb6dd3bbf22ce733
Andrey22 <andreymail22112006@gmail.com>
2025-05-17 19:14:50 +0300

    feat: #35 Сверстать страницу дропшиппинга

--- src/components/common/Footer/Information/Information.tsx
+++ src/components/common/Footer/Information/Information.tsx
@@ -37,7 +37,7 @@ const links: FooterLinksType[] = [
         group: 'Сотрудничество',
         links: [
             { link: '/wholesale', title: 'Оптом' },
-            { link: '/news', title: 'Дропшиппинг' },
+            { link: '/dropshipping', title: 'Дропшиппинг' },
         ],
     },
 ];
